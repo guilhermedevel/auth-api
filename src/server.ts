992be43cc3d9a8fae9f11cb2d12df8b67a1f22c5
guilhermedevel <guilherme.zarzur@gmail.com>
2026-01-20 09:08:19 -0300

    feat: wip server setup

---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import { InversifyExpressServer } from 'inversify-express-utils';
+
+//TODO: Controllers and their dependencies
+//TODO: Set inversify container
+//const server = new InversifyExpressServer(container)
